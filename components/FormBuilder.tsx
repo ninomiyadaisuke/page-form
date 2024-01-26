13048240e991205f5497f991ccd8582dcb84d27d
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2024-01-26 15:55:05 +0900

    Form builder

---
+++ components/FormBuilder.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Form } from "@prisma/client";
+import React from "react";
+import PreviewDialogBtn from "./PreviewDialogBtn";
+import SaveFormBtn from "./SaveFormBtn";
+import PublishFormBtn from "./PublishFormBtn";
+import Designer from "./Designer";
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import DragOverlayWrapper from "./DragOverlayWrapper";
+
+const FormBuilder = ({ form }: { form: Form }) => {
+  return (
+    <DndContext>
+      <main className="flex flex-col w-full">
+        <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+          <h1 className="truncate flex-col w-full">
+            <span className="text-muted-foreground mr-2">Form:</span>
+            {form.name}
+          </h1>
+          <div className="flex items-center gap-2">
+            <PreviewDialogBtn />
+            {!form.published && (
+              <>
+                <SaveFormBtn id={form.id} />
+                <PublishFormBtn id={form.id} />
+              </>
+            )}
+          </div>
+        </nav>
+        <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+          <Designer />
+        </div>
+      </main>
+      <DragOverlayWrapper />
+    </DndContext>
+  );
+};
+
+export default FormBuilder;
---
+++ components/FormElements.tsx
@@ -0,0 +1,39 @@
+import { TextFieldFormElement } from "./fields/TextField";
+
+export type ElementsType = "TextField";
+// | "TitleField"
+// | "SubTitleField"
+// | "ParagraphField"
+// | "SeparatorField"
+// | "SpacerField"
+// | "NumberField"
+// | "TextAreaField"
+// | "DateField"
+// | "SelectField"
+// | "CheckboxField";
+
+export type FormElement = {
+  type: ElementsType;
+  construct: (id: string) => FormElementInstance;
+  designerBtnElement: {
+    icon: React.ElementType;
+    label: string;
+  };
+  designerComponent: React.FC;
+  formComponent: React.FC;
+  propertiesComponent: React.FC;
+};
+
+export type FormElementInstance = {
+  id: string;
+  type: ElementsType;
+  extraAttributes: Record<string, any>;
+};
+
+type FormElementsType = {
+  [key in ElementsType]: FormElement;
+};
+
+export const FormElements: FormElementsType = {
+  TextField: TextFieldFormElement,
+};
