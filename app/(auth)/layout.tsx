07583dc8bff16d8ebabe104a1fadf621ff588c12
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2024-02-02 15:23:37 +0900

    deploy

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,14 +4,14 @@ import React, { ReactNode } from "react";
 
 function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen h-screen">
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
       <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
-        <h1 className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text">
-          PageForm
-        </h1>
-        <ThemeSwitcher />
+        <Logo />
+        <div className="flex gap-4 items-center">
+          <ThemeSwitcher />
+        </div>
       </nav>
-      <main className="flex w-full flex-grow">{children}</main>
+      <main className="flex w-full flex-grow h-full items-center justify-center">{children}</main>
     </div>
   );
 }
