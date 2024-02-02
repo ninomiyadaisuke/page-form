be8e9ca7716c4d50a40fd71c93b57224e194d16a
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2024-02-02 14:45:19 +0900

    submission table

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -7,6 +7,10 @@ import { LuView } from "react-icons/lu";
 import { FaWpforms } from "react-icons/fa";
 import { HiCursorClick } from "react-icons/hi";
 import { TbArrowBounce } from "react-icons/tb";
+import { ElementsType, FormElementInstance } from "@/components/FormElements";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { ReactNode } from "react";
+import { formatDistance } from "date-fns";
 
 async function FormDetailPage({
   params,
@@ -96,8 +100,100 @@ type Row = { [key: string]: string } & {
 async function SubmissionsTable({ id }: { id: number }) {
   const form = await GetFormWithSubmissions(id);
 
-  // if (!form) {
-  //   throw new Error("form not found");
+  if (!form) {
+    throw new Error("form not found");
+  }
+
+  const formElements = JSON.parse(form.content) as FormElementInstance[];
+
+  const columns: {
+    id: string;
+    label: string;
+    required: boolean;
+    type: ElementsType;
+  }[] = [];
+
+  formElements.forEach((element) => {
+    switch (element.type) {
+      case "TextField":
+        columns.push({
+          id: element.id,
+          label: element.extraAttributes.label,
+          required: element.extraAttributes.required,
+          type: element.type,
+        });
+        break;
+      default:
+        break;
+    }
+  });
+
+  const rows: Row[] = [];
+
+  form.FormSubmissions.forEach((submission) => {
+    const content = JSON.parse(submission.content);
+    rows.push({
+      ...content,
+      submittedAt: submission.createdAt,
+    });
+  });
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold my-4">Submissions</h1>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {columns.map((column) => (
+                <TableHead key={column.id} className="uppercase">
+                  {column.label}
+                </TableHead>
+              ))}
+              <TableHead className="text-muted-foreground text-right uppercase">
+                Submitted at
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow key={index}>
+                {columns.map((column) => (
+                  <RowCell
+                    key={column.id}
+                    type={column.type}
+                    value={row[column.id]}
+                  />
+                ))}
+                <TableCell className="text-muted-foreground text-right">
+                  {formatDistance(row.submittedAt, new Date(), {
+                    addSuffix: true,
+                  })}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
+}
+
+
+function RowCell({ type, value }: { type: ElementsType; value: string }) {
+  let node: ReactNode = value;
+
+  // switch (type) {
+  //   case "DateField":
+  //     if (!value) break;
+  //     const date = new Date(value);
+  //     node = <Badge variant={"outline"}>{format(date, "dd/MM/yyyy")}</Badge>;
+  //     break;
+  //   case "CheckboxField":
+  //     const checked = value === "true";
+  //     node = <Checkbox checked={checked} disabled />;
+  //     break;
   // }
-  return <h1 className="text-2xl font-bold my-4">Submissions</h1>;
+
+  return <TableCell>{node}</TableCell>;
 }
